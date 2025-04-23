a2c4092fe916e60dba0d4195e085608128afd0d9
alchemy_junior <athanasbrian99@gmail.com>
2025-04-23 22:44:58 +0300

    added search feature and began create listing form task

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,11 +10,16 @@ class ListingController extends Controller
 //Show all listings
     public function index(){
         //dd(request("tag"));
-        $listings=Listing::latest()->filter(request(["tag"]))->get();
+        $listings=Listing::latest()->filter(request(["tag","search"]))->get();
         return view('listings.index',["listings"=>$listings]);
     }
 //Show single listing
     public function show(Listing $listing){
         return view('listings.show',["listing"=>$listing]);
+    }
+
+    //Show create listing form
+        public function create(){
+        return view('listings.create',[]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ use App\Http\Controllers\ListingController;
 
 Route::get('/', [ListingController::class,"index"]);
 
+
+//show create listing form
+Route::get('listings/create', [ListingController::class,"create"]);
 Route::get('listings/{listing}', [ListingController::class,"show"]);
